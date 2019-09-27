98c6a1990fd745986b862ebcc04e196bfce492c2
leonardo cuquejo soares <leonardo.s@cuquejo.org>
2019-09-27 15:28:46 +0100

    ep5 fixes

--- app/src/Controller/ArticleController.php
+++ app/src/Controller/ArticleController.php
@@ -36,12 +36,14 @@ class ArticleController extends AbstractController
 Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
 lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
 labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
-turkey shank eu pork belly meatball non cupim.
+**turkey** shank eu pork belly meatball non cupim.
+
 Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur
 laboris sunt venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder,
 capicola biltong frankfurter boudin cupim officia. Exercitation fugiat consectetur ham. Adipisicing
-**picanha** shank et filet mignon pork belly ut ullamco. Irure velit turducken ground round doner incididunt
+picanha shank et filet mignon pork belly ut ullamco. Irure velit turducken ground round doner incididunt
 occaecat lorem meatball prosciutto quis strip steak.
+
 Meatball adipisicing ribeye bacon strip steak eu. Consectetur ham hock pork hamburger enim strip steak
 mollit quis officia meatloaf tri-tip swine. Cow ut reprehenderit, buffalo incididunt in filet mignon
 strip steak pork belly aliquip capicola officia. Labore deserunt esse chicken lorem shoulder tail consectetur
@@ -49,6 +51,7 @@ cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim ca
 fugiat.
 EOF;
 
+        dump($cache);die;
         $item = $cache->getItem('markdown_'.md5($articleContent));
         if (!$item->isHit()) {
             $item->set($markdown->transform($articleContent));
@@ -56,12 +59,11 @@ EOF;
         }
         $articleContent = $item->get();
 
-
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
-            'articleContent'=> $articleContent,
             'slug' => $slug,
             'comments' => $comments,
+            'articleContent' => $articleContent,
         ]);
     }
 
